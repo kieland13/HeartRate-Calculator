b850cd851892bd6fc62574030dffe8724cf0f250
Nick Kieland <nkieland@icloud.com>
2020-12-25 11:18:11 -0600

    first commit

---
+++ TargetHeartRateCalulator/HeartRates.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TargetHeartRateCalulator
+{
+   class HeartRates
+   {
+      //instance variables
+
+      private int dateOfBirth;
+
+      public string LastName { get; set; }
+
+      public string FirstName { get; set; }
+
+      public int DateOfBirth
+      {
+         get
+         {
+            return dateOfBirth;
+         }
+         set
+         {
+                if (value >= 1900 && value <= 2020)
+                dateOfBirth = value;
+         }
+      }
+
+      public HeartRates(string ln, string fn, int dob)
+      {
+         DateOfBirth = dob;
+         FirstName = fn;
+         LastName = ln;
+      }
+
+      public HeartRates()
+      {
+
+      }
+
+        public int CurrentYear = DateTime.Now.Year;
+      public int getCurrentAge()
+      {
+            return CurrentYear - DateOfBirth;
+      }
+      public int getMaxHeartRate()
+      {
+            return 220 - getCurrentAge();
+      }
+      public int getMinTargetHeartRate()
+      {
+            return Convert.ToInt32(getMaxHeartRate() * 0.5);
+      }
+      public int getMaxTargetHeartRate()
+      {
+            return Convert.ToInt32(getMaxHeartRate() * 0.85);
+      }
+   }
+}
+
---
+++ TargetHeartRateCalulator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace TargetHeartRateCalulator
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+             HeartRates hr1 = new HeartRates();
+
+         Console.WriteLine("Please enter first name: ");
+         hr1.FirstName = Console.ReadLine();
+
+         Console.WriteLine("Please enter last name: ");
+         hr1.LastName = Console.ReadLine();
+
+         Console.WriteLine("Please enter your year of birth: ");
+         hr1.DateOfBirth = Convert.ToInt32(Console.ReadLine());
+
+           
+
+         Console.WriteLine($"Your name is : {hr1.FirstName} { hr1.LastName}");
+         Console.WriteLine($"You age is: {hr1.getCurrentAge()}");
+         Console.WriteLine($"Your maximum heart rate is: {hr1.getMaxHeartRate()} ");
+         Console.WriteLine($"Your target max heart rate is: {hr1.getMaxTargetHeartRate()}");
+         Console.WriteLine($"Your target min heart rate is: {hr1.getMinTargetHeartRate()}");
+
+         Console.ReadKey();
+
+
+      }
+   }
+   }
